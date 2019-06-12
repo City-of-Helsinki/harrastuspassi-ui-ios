1b43e53c9d491a1640e9ee29722a66ec5754c1ea
Eetu Kallio <eetu.kallio@haltu.fi>
2019-06-12 10:35:11 +0300

    Initial Commit

---
+++ Harrastuspassi/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Harrastuspassi
+//
+//  Created by Eetu Kallio on 12/06/2019.
+//  Copyright © 2019 Haltu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Harrastuspassi/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Harrastuspassi
+//
+//  Created by Eetu Kallio on 12/06/2019.
+//  Copyright © 2019 Haltu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
