3a1c2db3a12801666452418f266214169ba7375b
Tiia Trogen <tiia.trogen@haltu.fi>
2019-07-25 17:50:07 +0300

    Ref: c-helsinki/harrastuspassi_ios#1 Add UI elements for Hobby detail view

---
+++ Harrastuspassi/Controllers/HobbyDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HobbyDetailViewController.swift
+//  Harrastuspassi
+//
+//  Created by Tiia Trogen on 25/07/2019.
+//  Copyright © 2019 Haltu. All rights reserved.
+//
+
+import UIKit
+
+class HobbyDetailViewController: UIViewController, UIScrollViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
